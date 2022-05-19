05f0499b5f84855441c8182ed11b72be6d727ff0
ReitoSerizawa <s.reitiger@gmail.com>
2022-05-19 09:19:22 -0400

    commit tree 4862869ff4b6549998eadcda099af8ea53cba261

---
+++ lib/song.rb
@@ -0,0 +1,39 @@
+class Song
+    attr_accessor :name, :artist, :genre
+
+    @@genres = []
+    @@artists = []
+    @@count = 0
+
+    def initialize (name, artist, genre)
+        @@count += 1
+        @@genres << genre
+        @@artists << artist
+
+        @name=name
+        @artist=artist
+        @genre=genre
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genre_count
+        @@genres.tally
+    end
+
+    def self.artist_count
+        @@artists.tally
+    end
+
+
+end
